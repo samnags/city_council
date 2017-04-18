143cf128abdba520e24ab0dd451a0486cdf44b05
samnags <sam.nagourney@gmail.com>
2017-04-18 14:16:11 -0700

    Refactor Member create

--- server/controllers/members.js
+++ server/controllers/members.js
@@ -1,20 +1,23 @@
 // refactor to destructring later
 const _ = require('lodash')
-const Member = require('../models').Member;
+const { Member } = require('../models');
 
 
 module.exports = {
   create(req, res) {
-    // let newMember = _.pick(req.body, 'firstName', 'lastName', 'party', 'firstDay', 'inOffice');
+    let newMember = _.pick(req.body, 'firstName', 'lastName', 'party', 'firstDay', 'inOffice');
+    console.log(newMember)
     return Member
-      .create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        party: req.body.party,
-        firstDay: req.body.firstDay,
-        inOffice: req.body.inOffice
-      })
+      .create(newMember)
       .then(member => res.status(201).send(member))
       .catch(error => res.status(400).send(error))
   }
 };
+
+// .create({
+//   firstName: req.body.firstName,
+//   lastName: req.body.lastName,
+//   party: req.body.party,
+//   firstDay: req.body.firstDay,
+//   inOffice: req.body.inOffice
+// })
