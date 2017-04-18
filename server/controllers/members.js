3654f53a30f9c8f15c0c4fe55e06c572151f8715
samnags <sam.nagourney@gmail.com>
2017-04-18 13:28:32 -0700

    Successful POST /api/members

--- server/controllers/members.js
+++ server/controllers/members.js
@@ -5,10 +5,16 @@ const Member = require('../models').Member;
 
 module.exports = {
   create(req, res) {
-    let newMember = _.pick(req.body, 'firstName', 'lastName', 'party', 'firstDay', 'inOffice');
+    // let newMember = _.pick(req.body, 'firstName', 'lastName', 'party', 'firstDay', 'inOffice');
     return Member
-      .create({newMember})
-      .then(member => res.status(201).send(todo))
+      .create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        party: req.body.party,
+        firstDay: req.body.firstDay,
+        inOffice: req.body.inOffice
+      })
+      .then(member => res.status(201).send(member))
       .catch(error => res.status(400).send(error))
   }
 };
