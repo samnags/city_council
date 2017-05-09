e115d8ae53a8421a40566a5e7713cdc5e40fe8bb
samnags <sam.nagourney@gmail.com>
2017-05-09 08:20:48 -0700

    fix server issues

---
+++ client/src/components.js/table.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import * as BS from 'react-bootstrap'
+
+class Table extends Component {
+    render() {
+        return (
+            <div>
+                <table>
+                </table>
+            </div>
+        );
+    }
+}
+
+export default Table;
